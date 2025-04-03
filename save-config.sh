2cb09172963eefdd9a623ae479763af85b469460
Justin Denning <jude9363@colorado.edu>
2025-04-03 21:03:43 +0000

    Work beginning for final project

--- save-config.sh
+++ save-config.sh
@@ -8,9 +8,9 @@ mkdir -p base_external/configs/
 make -C buildroot savedefconfig BR2_DEFCONFIG=${AESD_MODIFIED_DEFCONFIG_REL_BUILDROOT}
 
 if [ -e buildroot/.config ] && [ ls buildroot/output/build/linux-*/.config 1> /dev/null 2>&1 ]; then
-	grep "BR2_LINUX_KERNEL_CUSTOM_CONFIG_FILE" buildroot/.config > /dev/null
+	grep "BR2_PACKAGE_RPI_FIRMWARE_CONFIG_FILE" buildroot/.config > /dev/null
 	if [ $? -eq 0 ]; then
 		echo "Saving linux defconfig"
-		make -C buildroot linux-update-defconfig
+		make -C buildroot update-defconfig
 	fi
 fi
