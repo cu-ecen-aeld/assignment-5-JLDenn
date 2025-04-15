3a95f84f42d1a56fca51f04e06cb409c53de6404
Justin Denning <jude9363@colorado.edu>
2025-04-15 22:51:48 +0000

    All primary features are working

--- base_external/package/courseproject/courseproject.mk
+++ base_external/package/courseproject/courseproject.mk
@@ -4,7 +4,7 @@
 #
 ##############################################################
 
-COURSEPROJECT_VERSION = '7e11a40e02fd5ecc52775a6bbfff0ef1d0e8d2e3'
+COURSEPROJECT_VERSION = '768660ffa35e77346d75f85fa81841b76c33b6ef'
 COURSEPROJECT_SITE = 'git@github.com:cu-ecen-aeld/final-project-JLDenn.git'
 COURSEPROJECT_SITE_METHOD = git
 COURSEPROJECT_GIT_SUBMODULES = YES
@@ -17,6 +17,17 @@ define COURSEPROJECT_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0755 $(@D)/datactrl/datactrl $(TARGET_DIR)/root/
 	$(INSTALL) -m 0755 $(@D)/bt/*.sh $(TARGET_DIR)/root/
 	$(INSTALL) -m 0755 $(@D)/bt/*.exp $(TARGET_DIR)/root/
+	
+	$(INSTALL) -m 0644 $(@D)/web/*.html $(TARGET_DIR)/var/www/
+	
+	$(INSTALL) -d 0755 $(@D)/web/cgi-bin/ $(TARGET_DIR)/var/www/cgi-bin/
+	$(INSTALL) -m 0755 $(@D)/web/cgi-bin/* $(TARGET_DIR)/var/www/cgi-bin/
+	
+	$(INSTALL) -d 0755 $(@D)/web/css/ $(TARGET_DIR)/var/www/css/
+	$(INSTALL) -m 0644 $(@D)/web/css/* $(TARGET_DIR)/var/www/css/
+	
+	$(INSTALL) -d 0755 $(@D)/web/js/ $(TARGET_DIR)/var/www/js/
+	$(INSTALL) -m 0755 $(@D)/web/js/* $(TARGET_DIR)/var/www/js/
 endef
 
 $(eval $(generic-package))
